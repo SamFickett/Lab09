a84a6d39b62c636660d7f18056d7748cd1ff09ca
Nikan <nikankad@protonmail.com>
2023-11-27 14:44:36 -0500

    uploaded max heap

--- MaxHeapMaker.java
+++ MaxHeapMaker.java
@@ -1,15 +1,40 @@
+//author: Nikan Kadkhodazadeh
 package Lab9;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Collections;
 
 public class MaxHeapMaker {
     /**
-     * This method takes in an array of integers and returns a max-heap 
+     * This method takes in an array of integers and returns a max-heap
+     *
      * @param input array of integer values
      * @return input as a max-heap
      */
-    public static int[]heapify(int[]input)
-    {
-        // Your code goes here
-        
+    public static int[] heapify(int[] input) {
+        //make it a priority queue but reverse it to make it a max heap
+        PriorityQueue<Integer> prq = new PriorityQueue<>(Collections.reverseOrder());
+        //add all elements in the heap
+        for (int in : input) {
+            prq.add(in);
+        }
+
+//            make a temp integer array
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+//              add them in
+        for (int in : input) {
+            temp.add(prq.poll());
+        }
+        //map the array list to the input array
+        input = temp.stream().mapToInt(i -> i).toArray();
+
         return input;
+    }
+
+    public static void main(String[] args) {
+        int[] myInts = {6, 2, 4, 17, 5, 13, 1};
+        heapify(myInts);
     }
 }
